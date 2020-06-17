9ee37ac5cc65277117a2c59f394c3dba1a8d4f4d
Mike Esposito <mikeesposito6852@gmail.com>
2020-06-17 14:53:38 -0700

    Remove empty Player class and move Code class to top of file

--- mastermind.rb
+++ mastermind.rb
@@ -1,6 +1,52 @@
 require 'colorize'
 
-class Player
+class Code
+
+  @@COLOR_LIST = ["r", "y", "g", "c", "b", "m"]
+
+  # create a randomized code
+  def create_random_code
+    random_code = []
+    0.upto(3) { random_code.push(rand(6)) }
+    random_code.each_with_index { |num, i| random_code[i] = @@COLOR_LIST[num] }
+  end
+
+  # print request for a code from user
+  def request_code(type)
+    choose = type == "guess" ? "Guess" : "Select"
+    choice = type == "guess" ? "guess" : "selection"
+    puts <<~HEREDOC
+      #{choose} four colors from the following list
+      #{"red".light_red}, #{"yellow".light_yellow}, #{"green".light_green}, \
+      #{"cyan".light_cyan}, #{"blue".light_blue}, and #{"magenta".light_magenta}
+      Use the first letter of each color (in lower case) for your #{choice}
+      e.g. rygc or rrbb
+
+    HEREDOC
+  end
+
+  # check that a code has exactly four colors
+  def length_correct?(user_guesses)
+    user_guesses.length == 4
+  end
+
+  # check that a code has only valid colors
+  def colors_correct?(user_guesses)
+    user_guesses.all? { |color| @@COLOR_LIST.include?(color) }
+  end
+
+  # obtain a code from the user, reprompting until it is valid
+  def get_user_code(type)
+    user_code = []
+    request_code(type)
+    choices = type == "guess" ? "guesses" : "selections"
+    until length_correct?(user_code) && colors_correct?(user_code)
+      puts "Please enter your four #{choices}."
+      user_code = gets.chomp.downcase.split("")
+    end
+    user_code
+  end
+
 end
 
 class Round
@@ -74,55 +120,6 @@ class Round
     find_exact_matches(guesses, solution)
     find_color_only_matches(guesses, solution)
     display_score(guesses, solution, game_version)
-  end
-
-end
-
-class Code
-
-  @@COLOR_LIST = ["r", "y", "g", "c", "b", "m"]
-
-  # create a randomized code
-  def create_random_code
-    random_code = []
-    0.upto(3) { random_code.push(rand(6)) }
-    random_code.each_with_index { |num, i| random_code[i] = @@COLOR_LIST[num] }
-  end
-  
-  # print request for a code from user
-  def request_code(type)
-    choose = type == "guess" ? "Guess" : "Select"
-    choice = type == "guess" ? "guess" : "selection"
-    puts <<~HEREDOC
-      #{choose} four colors from the following list
-      #{"red".light_red}, #{"yellow".light_yellow}, #{"green".light_green}, \
-      #{"cyan".light_cyan}, #{"blue".light_blue}, and #{"magenta".light_magenta}
-      Use the first letter of each color (in lower case) for your #{choice}
-      e.g. rygc or rrbb
-
-    HEREDOC
-  end
-
-  # check that a code has exactly four colors
-  def length_correct?(user_guesses)
-    user_guesses.length == 4
-  end
-
-  # check that a code has only valid colors
-  def colors_correct?(user_guesses)
-    user_guesses.all? { |color| @@COLOR_LIST.include?(color) }
-  end
-
-  # obtain a code from the user, reprompting until it is valid
-  def get_user_code(type)
-    user_code = []
-    request_code(type)
-    choices = type == "guess" ? "guesses" : "selections"
-    until length_correct?(user_code) && colors_correct?(user_code)
-      puts "Please enter your four #{choices}."
-      user_code = gets.chomp.downcase.split("")
-    end
-    user_code
   end
 
 end
